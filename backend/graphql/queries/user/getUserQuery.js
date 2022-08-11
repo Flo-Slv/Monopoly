e8c9d9f8a532a419e28dcd2eb52fa6dccaa2eccc
Flo Slv <florent3969@hotmail.fr>
2022-08-11 21:31:44 +0200

    🟦 feat: Create query getUser to get one user by email

--- backend/graphql/queries/user/getUserQuery.js
+++ backend/graphql/queries/user/getUserQuery.js
@@ -1,8 +1,10 @@
 import User from '../../../db/models/User.js';
 
-const getUserQuery = async () => {
+const getUserQuery = async args => {
+	const { email } = args;
+
 	try {
-		return [];
+		return await User.findOne({ email: email });
 	}
 	catch(err) {
 		throw new Error(err);
