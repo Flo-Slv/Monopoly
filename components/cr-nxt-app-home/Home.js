3146e793ebec8b9da0bf3d2cfa881b9b75cd04b7
kameamea133 <monnier1977@gmail.com>
2022-08-13 22:41:25 +0200

    add new-user.js file to Home page for test

--- components/cr-nxt-app-home/Home.js
+++ components/cr-nxt-app-home/Home.js
@@ -1,43 +1,16 @@
 import Image from 'next/image';
 import VercelLogo from './vercel.svg';
 import styles from './Home.module.css';
+import FormNewUser from '../../pages/auth/new-user';
+import AuthBackground from '../auth/authBackground.jpg';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Image alt="" src={AuthBackground} placeholder="blur" layout="fill" objectFit="cover" style={{ zIndex: '-3'}} />
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+      <FormNewUser />
       </main>
 
       <footer className={styles.footer}>
