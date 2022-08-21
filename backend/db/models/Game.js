b7891192ead039ab438dbe65503aa15216e45323
Marie Briand <mariebriandbzh@gmail.com>
2022-08-21 22:04:26 +0200

    fix bug modif _id

--- backend/db/models/Game.js
+++ backend/db/models/Game.js
@@ -3,41 +3,47 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const gameSchema = new Schema({
-	// start with
-	createdAt: {
-		type: String,
-		required: Boolean(true)
-	},
-	attendees: [{
-		id: String,
-		username: String,
-		urlAvatar: String
-		// money: Number
-	}],
-	chatbox: [{
-		player: {
-			id: String,
-			username: String,
-			urlAvatar: String
-		},
-		text: String,
-		time: String
-	}],
-	// generate after launch game
-	squares: [{
-		id: String,
-		label: String,
-		type: String,
-		group: String,
-		players: [{ id: String, username: String, urlAvatar: String }],
-		owner: { id: String, username: String, urlAvatar: String },
-		nbHouses: Number,
-		nbHostels: Number,
-		mortgaged: Boolean,
-		monopoly: Boolean
-	}],
-	startTime: String,
-	endTime: String
+    // start with
+    createdAt: {
+        type: String,
+        required: Boolean(true),
+    },
+    attendees: [
+        {
+            id: String,
+            username: String,
+            urlAvatar: String,
+            // money: Number
+        },
+    ],
+    chatbox: [
+        {
+            player: {
+                id: String,
+                username: String,
+                urlAvatar: String,
+            },
+            text: String,
+            time: String,
+        },
+    ],
+    // generate after launch game
+    squares: [
+        {
+            id: String,
+            label: String,
+            type: String,
+            group: String,
+            players: [{ id: String, username: String, urlAvatar: String }],
+            owner: { id: String, username: String, urlAvatar: String },
+            nbHouses: Number,
+            nbHostels: Number,
+            mortgaged: Boolean,
+            monopoly: Boolean,
+        },
+    ],
+    startTime: String,
+    endTime: String,
 });
 
 export default mongoose.models.Game || mongoose.model('Game', gameSchema);
--- backend/graphql/resolvers/games.js
+++ backend/graphql/resolvers/games.js
@@ -1,5 +1,5 @@
 import getGamesQuery from '../queries/game/getGamesQuery.js';
-import getGameQuery	 from '../queries/game/getGameQuery.js';
+import getGameQuery from '../queries/game/getGameQuery.js';
 
 import addGameMutation from '../mutations/game/addGameMutation.js';
 import joinGameMutation from '../mutations/game/joinGameMutation.js';
@@ -7,28 +7,28 @@ import leaveGameMutation from '../mutations/game/leaveGameMutation.js';
 import launchGameMutation from '../mutations/game/launchGameMutation.js';
 
 const gamesResolvers = {
-	Query: {
-		getGames: async () => {
-			return await getGamesQuery();
-		},
-		getGame: async (_, args) => {
-			return await getGameQuery(args);
-		}
-	},
-	Mutation: {
-		addGame: async (_, __, context) => {
-			return await addGameMutation(context);
-		},
-		joinGame: async (_, args, context) => {
-			return await joinGameMutation(args, context);
-		},
-		leaveGame: async (_, args, context) => {
-			return await leaveGameMutation(args, context);
-		},
-		launchGame: async (_, args, context) => {
-			return await launchGameMutation(args, context);
-		}
-	}
+  Query: {
+    getGames: async () => {
+      return await getGamesQuery();
+    },
+    getGame: async (_, args) => {
+      return await getGameQuery(args);
+    },
+  },
+  Mutation: {
+    addGame: async (_, __, context) => {
+      return await addGameMutation(context);
+    },
+    joinGame: async (_, args, context) => {
+      return await joinGameMutation(args, context);
+    },
+    leaveGame: async (_, args, context) => {
+      return await leaveGameMutation(args, context);
+    },
+    launchGame: async (_, args, context) => {
+      return await launchGameMutation(args, context);
+    },
+  },
 };
 
 export default gamesResolvers;
